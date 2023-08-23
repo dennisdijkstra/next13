081e3bd7b50447c804908f6de647d81a7212e06c
Dennis Dijkstra <mail@dennisdijkstra.com>
2023-08-23 18:57:28 +0200

    Reinstall node types on client

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -2,14 +2,14 @@ import jwt from 'jsonwebtoken'
 import { Request, Response, NextFunction } from 'express'
 
 export const authorize = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers.token
+  const accessToken = req.cookies.access_token
 
-  if (! token) {
+  if (! accessToken) {
     return res.status(403).send('A token is required for authentication')
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.TOKEN_KEY)
+    const decoded = jwt.verify(accessToken, process.env.JWT_ACCESS_TOKEN_SECRET)
     req.body.user = decoded
 
     next()
