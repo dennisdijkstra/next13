c380c7024413790b3bee3ef55f94d4f39c633cbf
Dennis Dijkstra <mail@dennisdijkstra.com>
2023-11-06 20:04:02 +0100

    Use gsap to animate modal overlay

--- client/src/app/components/Modal.tsx
+++ client/src/app/components/Modal.tsx
@@ -3,6 +3,7 @@
 import { ReactNode, useState, useEffect, useRef, MouseEvent } from 'react'
 import { createPortal } from 'react-dom'
 import { X } from '@phosphor-icons/react'
+import gsap from 'gsap'
 
 type ModalProps = {
   title: string
@@ -17,9 +18,17 @@ export default function Modal({
   onCancel,
   children,
 }: ModalProps) {
+  const overlayRef = useRef<HTMLDivElement>(null)
   const modalRef = useRef<HTMLDivElement>(null)
   const buttonRef = useRef<HTMLButtonElement>(null)
   const [isMounted, setIsMounted] = useState(false)
+
+  const animate = (isOpen: boolean) => {
+    if (overlayRef.current) {
+      const opacity = isOpen ? 0.5 : 0
+      gsap.to(overlayRef.current, { opacity, duration: 0.3 })
+    }
+  }
 
   const close = (e: MouseEvent<HTMLElement>) => {
     e.preventDefault()
@@ -28,27 +37,32 @@ export default function Modal({
       return
     }
 
-    onCancel()
+    animate(false)
+    setTimeout(() => onCancel(), 100)
   }
 
   useEffect(() => {
     setIsMounted(true)
+    setTimeout(() => animate(true), 0)
   }, [])
 
   return (
     isMounted ? (
       createPortal(
-        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black/50" onClick={close}>
-          <div className="relative w-[600px] h-[400px]" ref={modalRef}>
-            <div className="w-full h-full p-10 rounded-md bg-white">
-              <button className="absolute top-5 right-5" onClick={close} ref={buttonRef}>
-                <X size={24} weight="bold" />
-              </button>
-              {title && <h1 className="text-3xl text-center font-bold">{title}</h1>}
-              <div className="mt-10">{children}</div>
+        <>
+          <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0" ref={overlayRef} onClick={close} />
+          <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+            <div className="relative w-[600px] h-[400px]" ref={modalRef}>
+              <div className="w-full h-full p-10 rounded-md bg-white">
+                <button className="absolute top-5 right-5" onClick={close} ref={buttonRef}>
+                  <X size={24} weight="bold" />
+                </button>
+                {title && <h1 className="text-3xl text-center font-bold">{title}</h1>}
+                <div className="mt-10">{children}</div>
+              </div>
             </div>
           </div>
-        </div>, document.getElementById('modal-root')!)
+        </>, document.getElementById('modal-root')!)
     ) : null
   )
 }
